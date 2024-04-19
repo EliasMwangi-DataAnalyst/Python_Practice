37a1b2db9819720a5539c8d2dc75adf938792b64
EliasMwangi-DataAnalyst <157376395+EliasMwangi-DataAnalyst@users.noreply.github.com>
2024-04-19 11:58:17 +0300

    Add files via upload
    creating a list in python using functions like append, insert and extend

---
+++ my_list.py
@@ -0,0 +1,20 @@
+# create an empty list called my_list
+my_list = []
+
+# append elements to the list
+my_list.append(18)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+print(my_list)
+
+# insert the value 15 at the second position
+my_list.insert(1, 15)
+
+print(my_list)
+
+# extend my_list with another list [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+print(my_list)
